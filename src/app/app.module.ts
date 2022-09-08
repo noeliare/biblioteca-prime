f03a3d6806ef5cd37547e2cbae033d8640f1517c
Sandra Noelia Rodriguez Escobar <sandranoeliarodriguezescobar@gmail.com>
2022-09-08 17:50:09 -0400

    Tarea 1 Angular parte 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
-import {MenubarModule} from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 import { LibrosComponent } from './libros/libros.component';
-import { AutoresComponent } from './autores/autores.component'
+import { AutoresComponent } from './autores/autores.component';
+import { HttpClientModule} from '@angular/common/http';
+import { TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { AutoresComponent } from './autores/autores.component'
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/libros/libros.component.ts
+++ src/app/libros/libros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Libro } from '../interfaces/libro.interfaces';
+import { LibrosService } from '../servicios/libros.service';
 
 @Component({
   selector: 'app-libros',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibrosComponent implements OnInit {
 
-  constructor() { }
+  listaLibros: Libro[] = [];
+  cargando: boolean = false;
+  
+  constructor(
+    private servioLibros: LibrosService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarLibros();
+  }
+
+  cargarLibros(): void{
+    this.cargando = true;
+    this.servioLibros.get().subscribe({
+      next: (datos) =>{
+        this.listaLibros = datos;
+        this.cargando = false;
+      },
+      error: (e) => {
+        console.log(e);
+        this.cargando = false;
+      }
+    });
   }
 
 }
